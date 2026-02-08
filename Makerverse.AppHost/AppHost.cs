1ab1d778c810d7ea9ebd831bc4545c5b38d7f136
Stars sea <Stars_sea@outlook.com>
2026-02-08 20:22:59 +0800

    feat: update Keycloak configuration and add YARP gateway routes

--- Makerverse.AppHost/AppHost.cs
+++ Makerverse.AppHost/AppHost.cs
@@ -1,16 +1,21 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-#pragma warning disable ASPIRECERTIFICATES001
-var keycloak = builder.AddKeycloak("keycloak", 6001)
-    .WithDataVolume("keycloak-data")
-    // To solve health check issues
-    .WithAnnotation(new HttpsCertificateAnnotation {
-        UseDeveloperCertificate = false
-    });
-#pragma warning restore ASPIRECERTIFICATES001
+var compose = builder.AddDockerComposeEnvironment("production")
+    .WithDashboard(dashboard => dashboard.WithHostPort(8080));
 
 var postgres = builder.AddPostgres("postgres", port: 5432)
     .WithDataVolume("postgres-data");
+
+// var keycloakDb = postgres.AddDatabase("keycloak-db");
+
+var keycloak = builder.AddKeycloak("keycloak", 6001)
+    .WithDataVolume("keycloak-data")
+    .WithEnvironment("KC_HTTP_ENABLED", "true")
+    .WithEnvironment("KC_HOSTNAME_STRICT", "false")
+    // .WithPostgres(keycloakDb, xaEnabled: true)
+    .WithHttpEndpoint(port: 6001, targetPort: 8080, "keycloak")
+    .WithExternalHttpEndpoints();
+    // .WaitFor(keycloakDb);
 
 var typesenseApiKey = builder.AddParameter("typesense-api-key", secret: true);
 
@@ -54,5 +59,14 @@ var searchService = builder.AddProject<Projects.SearchService>("search-svc")
     .WithReference(rabbitmq)
     .WaitFor(typesense)
     .WaitFor(rabbitmq);
+
+var yarp = builder.AddYarp("gateway")
+    .WithConfiguration(yarpBuilder => {
+        yarpBuilder.AddRoute("/activities/{**catch-all}", activityService);
+        yarpBuilder.AddRoute("/tags/{**catch-all}", activityService);
+        yarpBuilder.AddRoute("/lives/{**catch-all}", liveService);
+        yarpBuilder.AddRoute("/search/{**catch-all}", searchService);
+    })
+    .WithHostPort(8001);
 
 builder.Build().Run();
